37c739f4de843bb2db27db1ce7b431439b739ddc
Esmelyn04 <EML_JOSE_BAEZ@HOTMAIL.COM>
2024-05-07 20:07:29 -0400

    Create a server with express, create a express app and add port 3000

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// import express into our file
+const express = require('express')
+
+// create a new express applicaction
+const app = express()
+
+// define the port our server will run on
+const port = 3000
