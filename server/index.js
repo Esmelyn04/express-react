33913529c3a6d7d545f0786f1416c53eff2b5766
Esmelyn04 <EML_JOSE_BAEZ@HOTMAIL.COM>
2024-05-07 20:52:19 -0400

    Define our server routes /message & /test and run our server to listen at the port we defined

--- server/index.js
+++ server/index.js
@@ -6,3 +6,24 @@ const app = express()
 
 // define the port our server will run on
 const port = 3000
+
+// define our server routes
+app.get('/message', (req, res)=>{
+    // do something when the server processes this request
+
+    // send back a response to the client
+    res.send("Hello from our server!!!!")
+
+})
+
+app.get('/test', (req, res)=>{
+    // do something when the server processes this request
+    res.send("Server is operational")
+})
+
+// run our server to listen at the port we defined
+app.listen(port, ()=>{
+    console.log(`Server in running on port ${port}`)
+})
+
+
